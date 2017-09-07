19a87ea4d4a83d24c292130684c6c244f56724b4
papillon <devopreet.ch@gmail.com>
2017-09-07 08:43:49 -0500

    smoothed plane flying

--- axel.js
+++ axel.js
@@ -338,9 +338,13 @@ function updatePlane(){
     var targetX = normalize(mousePos.x, -1, 1, -100, 100);
     var targetY = normalize(mousePos.y, -1, 1, 25, 175);
 
-    // update the airplane's position
-    airplane.mesh.position.y = targetY;
-    airplane.mesh.position.x = targetX;
+    //updated
+    airplane.mesh.position.x += (targetX-airplane.mesh.position.x)*0.1;
+    airplane.mesh.position.y += (targetY-airplane.mesh.position.y)*0.1;
+
+    airplane.mesh.rotation.x = (airplane.mesh.position.y-targetY)*0.0064;
+    airplane.mesh.rotation.z = (targetY-airplane.mesh.position.y)*0.0128;
+
     airplane.propeller.rotation.x += 0.3;
 }
 
